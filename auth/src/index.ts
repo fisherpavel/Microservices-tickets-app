b8ad18a6d325bcd489df20e3085944b74785e00a
fisherpavel <fisherpavel@bk.ru>
2020-07-16 08:40:22 +0700

    add connecting to mongodb

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import 'express-async-errors'
 import {json} from 'body-parser'
+import mongoose from 'mongoose'
 
 import {currentUserRouter} from './routes/current-user'
 import {signinRouter} from './routes/signin'
@@ -23,7 +24,20 @@ app.all('*', async (req, res) => {
 
 app.use(errorHandler)
 
+const start = async () => {
+    try{
+    await mongoose.connect(`mongodb://auth-mongo-srv:27017/auth`, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true
+    })
+    console.log('Connecting to mongodb')
+    } catch(err){
+        console.error(err)
+    }
+    app.listen(3000, () => {
+        console.log('Listening on port 3000')
+    })
+}
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000')
-})
+start()
