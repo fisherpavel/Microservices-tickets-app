6fd846a4a4229200d69ea9628da44da48dffcbec
fisherpavel <fisherpavel@bk.ru>
2020-07-17 12:04:58 +0700

    access Env Variables jwt

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -33,17 +33,20 @@ app.all('*', async (req, res) => {
 app.use(errorHandler)
 
 const start = async () => {
+        if(!process.env.JWT_KEY){
+            throw new Error('JWT_KEY must be defined')
+        }
     try{
-    await mongoose.connect(`mongodb://auth-mongo-srv:27017/auth`, {
+        await mongoose.connect(`mongodb://auth-mongo-srv:27017/auth`, {
         useUnifiedTopology: true,
         useNewUrlParser: true,
         useCreateIndex: true
     })
-    console.log('Connecting to mongodb')
+        console.log('Connecting to mongodb')
     } catch(err){
         console.error(err)
     }
-    app.listen(3000, () => {
+        app.listen(3000, () => {
         console.log('Listening on port 3000')
     })
 }
