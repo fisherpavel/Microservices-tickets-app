f45a6a24f87892d334a6d2b8624847b668896873
fisherpavel <fisherpavel@bk.ru>
2020-07-18 09:51:20 +0700

    add current-user logic

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,10 +1,20 @@
-import express, {Router} from 'express'
+import {Router} from 'express'
+import jwt from 'jsonwebtoken'
 
 const router = Router()
 
-
 router.get('/api/users/currentuser', (req, res) => {
+    if(!req.session?.jwt){
+        return res.send({currentUser: null})
+    }
 
+    try{
+        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!)
+        res.send({currentUser: payload})
+    } catch(err){
+        res.send({currentUser: null})
+    }
+    
 })
 
 
