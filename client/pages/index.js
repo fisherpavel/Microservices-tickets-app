dd503ee6a0863c79e66f13c969052e467dac6cf9
fisherpavel <fisherpavel@bk.ru>
2020-07-20 13:47:57 +0700

    fetch data during

---
+++ client/pages/index.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+
+const LandingPage = ({currentUser}) => {
+    console.log(currentUser)
+    return <h1>Landing Page</h1>
+}
+
+LandingPage.getInitialProps = async ({req}) => {
+   if(typeof window === 'undefined'){
+        const {data} = await axios.get(
+            `http://ingress-nginx.ingress-nginx.svc.cluster.local/api/users/currentuser`, {
+                headers: req.headers
+            }
+        )
+        return data
+   } else {
+       const {data} = await axios.get('/api/users/currentuser')
+       return data
+   }
+}
+
+
+export default LandingPage
