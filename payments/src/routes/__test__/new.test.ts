40bdba41f8e6453c2f276e32af975d862cf96871
fisherpavel <fisherpavel@bk.ru>
2020-08-08 10:44:13 +0700

    implement tying an order and charge

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -4,6 +4,7 @@ import {app} from '../../app'
 import {Order} from '../../models/order'
 import { OrderStatus } from '@sgtickets/common'
 import {stripe} from '../../stripe'
+import { Payment } from '../../models/payment'
 
 it('returns a 404 when purchasing an order that does not exist', async () => {
     await request(app)
@@ -87,4 +88,9 @@ it('returns a 201 with valid inputs', async () => {
     expect(stripeCharge).toBeDefined()
     expect(stripeCharge!.currency).toEqual('usd')
     
+    const payment = await Payment.findOne({
+        orderId: order.id,
+        stripeId: stripeCharge!.id
+    })
+    expect(payment).not.toBeNull()
 })
