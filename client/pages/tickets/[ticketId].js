ef1b6abb6913af598029b8b27831fecf6595ca6c
fisherpavel <fisherpavel@bk.ru>
2020-08-12 08:56:01 +0700

    implement orderShow component

--- client/pages/tickets/[ticketId].js
+++ client/pages/tickets/[ticketId].js
@@ -1,3 +1,4 @@
+import Router from 'next/router'
 import useRequest from '../../hooks/use-request'
 
 const ticketShow = ({ticket}) => {
@@ -5,7 +6,7 @@ const ticketShow = ({ticket}) => {
         url: '/api/orders',
         method: 'post',
         body: {ticketId: ticket.id},
-        onSuccess: (order) => console.log(order)
+        onSuccess: (order) => Router.push('/orders/[orderId]', `/orders/${order.id}`)
     })
 
     return (
