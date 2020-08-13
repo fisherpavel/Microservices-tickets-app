c33da16d58dfb6377c491b00a931b32761c6200a
fisherpavel <fisherpavel@bk.ru>
2020-08-13 08:34:35 +0700

    implement paying for an order

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,7 +1,17 @@
 import {useEffect, useState} from 'react'
+import StripeCheckout from 'react-stripe-checkout'
+import useRequest from '../../hooks/use-request'
 
-const OrderShow = ({order}) => {
+const OrderShow = ({order, currentUser}) => {
     const [timeLeft, setTimeLeft] = useState('')
+    const {doRequest, errors} = useRequest({
+        url: '/api/payments',
+        method: 'post',
+        body: {
+            orderId: order.id
+        },
+        onSuccess: (payment) => console.log(payment)
+    })
 
     useEffect(() => {
         const findTimeLeft = () => {
@@ -20,7 +30,16 @@ const OrderShow = ({order}) => {
         return <div>Order expired</div>
     }
 
-    return <div>Time left to pay: {timeLeft} seconds</div>
+    return <div>
+        Time left to pay: {timeLeft} seconds
+        <StripeCheckout
+            token={({id}) => doRequest({token: id}) }
+            stripeKey="pk_test_51HDgTjA2XXfbQpl5n6UPKynyXqTgu0MXiZvkxk9Uayl3gnB4UXvZreGFTAZo3NvQjcYfeWQ2zmK0Y0r9EcEonUpl006ZSeFKGw"
+            amount={order.ticket.price * 100}    
+            email={currentUser.email} 
+        />
+        {errors}
+        </div>
 }
 
 OrderShow.getInitialProps = async (context, client) => {
